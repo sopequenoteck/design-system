42fbdb05deeacf14308d094a9d4e2063cedd345b
sopequenoteck <sopequeno.tech@gmail.com>
2025-12-05 15:07:06 +0100

    Configure Storybook setup for docs and assets

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,28 @@
 import type { StorybookConfig } from '@storybook/angular';
+import path from 'path';
 
 const config: StorybookConfig = {
   stories: [
-    '../projects/ds-angular/src/**/*.mdx',
-    '../projects/ds-angular/src/**/*.stories.@(js|jsx|mjs|ts|tsx)'
+    {
+      directory: '../projects/ds-angular/src/lib',
+      files: '**/*.mdx'
+    },
+    {
+      directory: '../projects/ds-angular/src/lib',
+      files: '**/*.stories.@(js|jsx|mjs|ts|tsx)'
+    }
   ],
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-links',
-    '@storybook/addon-a11y'
+    '@storybook/addon-a11y',
+    'storybook/experimental-addon-test'
   ],
+  staticDirs: ['../projects/ds-angular/src/public'],
+  docs: {
+    autodocs: true
+  },
   framework: {
     name: '@storybook/angular',
     options: {
@@ -20,7 +32,21 @@ const config: StorybookConfig = {
   core: {
     disableTelemetry: true
   },
+  webpackFinal: async (webpackConfig) => {
+    const alias = webpackConfig.resolve?.alias ?? {};
 
+    return {
+      ...webpackConfig,
+      resolve: {
+        ...webpackConfig.resolve,
+        alias: {
+          ...alias,
+          '@ds/utils': path.resolve(__dirname, '../projects/ds-angular/src/lib/utils'),
+          '@ds/themes': path.resolve(__dirname, '../projects/ds-angular/src/styles/themes')
+        }
+      }
+    };
+  }
 };
 
 export default config;
